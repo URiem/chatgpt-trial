7b567317cbbfe10ecd45fe064e7b8e98a71818f5
URiem <uriemenschneider@gmail.com>
2023-04-18 18:24:45 +0000

    Try different api key

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import openai
 
-openai.api_key = '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'
+# openai.api_key = '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'
+openai.api_key = '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'
 
 messages = [ {"role": "system", "content": 
               "You are a intelligent assistant."} ]
