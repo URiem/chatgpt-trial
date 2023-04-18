17abd9f9ceaa092bb54fb603497b1ea7c98f1d6d
URiem <uriemenschneider@gmail.com>
2023-04-18 17:38:10 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import openai
+
+openai.my_api_key = ''
+
+messages = [ {"role": "system", "content": 
+              "You are a intelligent assistant."} ]
+while True:
+    message = input("User : ")
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT: {reply}")
+    messages.append({"role": "assistant", "content": reply})
